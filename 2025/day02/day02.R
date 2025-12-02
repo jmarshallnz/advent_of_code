97c5131aec4009248ad8f07cfeaa89c718552d27
Jonathan Marshall <jcmarsha@gmail.com>
2025-12-02 20:50:37 +1300

    day 2 done

---
+++ 2025/day02/day02.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+
+# part 1:
+read.table("2025/day02/input.txt", sep=",") |>
+  pivot_longer(everything(), names_to="range", values_to="minmax") |>
+  extract(minmax, into=c('from', 'to'), regex="([0-9]+)\\-([0-9]+)", convert=TRUE) |>
+  mutate(digits_from=ceiling(log10(from+0.1)), digits_to = ceiling(log10(to+0.1))) |>
+  mutate(from_fix=if_else((digits_from %% 2) != 0, 10^digits_from, from),
+         to_fix=if_else((digits_to %% 2) != 0, 10^(digits_to-1)-1, to)) |>
+  filter(from_fix <= to_fix) |>
+  mutate(digits_from=ceiling(log10(from_fix+0.1)), digits_to = ceiling(log10(to_fix+0.1))) |>
+  mutate(from_pat = from_fix %/% 10^(digits_from/2),
+         to_pat =   to_fix %/% 10^(digits_to/2)) |>
+  mutate(range = map2(from_pat, to_pat, seq)) |>
+  unnest(range) |>
+  mutate(range = as.numeric(paste(range, range, sep=''))) |>
+  filter(range >= from & range <= to) |>
+  summarise(sum(range))
+
+# part 2: same idea, just we need to do it for 2, 3, 4, ... 10
+# only trick bit is getting the right power out of it
+find_invalids <- function(dat, rep_times=2) {
+  dat |>
+    mutate(from_fix=if_else((digits_from %% rep_times) != 0, 10^digits_from, from),
+           to_fix=if_else((digits_to %% rep_times) != 0, 10^(digits_to-1)-1, to)) |>
+    filter(from_fix <= to_fix) |>
+    mutate(digits_from=ceiling(log10(from_fix+0.1)), digits_to = ceiling(log10(to_fix+0.1))) |>
+    mutate(from_pat = from_fix %/% 10^(digits_from*(1 - 1/rep_times)),
+           to_pat =   to_fix %/% 10^(digits_to*(1 - 1/rep_times))) |>
+    mutate(range = map2(from_pat, to_pat, seq)) |>
+    unnest(range) |>
+    mutate(range = map_dbl(range, \(x) as.numeric(paste(rep(x, rep_times), collapse='')))) |>
+    filter(range >= from & range <= to)
+}
+
+dat |> find_invalids(rep_times=3) |> print(n=100)
+dat |> find_invalids(rep_times=4) |> print(n=100)
+
+map_dfr(2:10, \(x) { dat |> find_invalids(x) }) |>
+  pull(range) |> unique() |>
+  sum()
