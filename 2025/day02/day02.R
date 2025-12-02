536d5786a028f2aa2c2f8262bcde8d872df15c03
Jonathan Marshall <jcmarsha@gmail.com>
2025-12-03 09:01:23 +1300

    adds another part 2 solution for day 2

--- 2025/day02/day02.R
+++ 2025/day02/day02.R
@@ -18,7 +18,8 @@ read.table("2025/day02/input.txt", sep=",") |>
   summarise(sum(range))
 
 # part 2: same idea, just we need to do it for 2, 3, 4, ... 10
-# only trick bit is getting the right power out of it
+# only trick bit is getting the right power out of it.
+# possibly 'easier' to operate on strings?
 find_invalids <- function(dat, rep_times=2) {
   dat |>
     mutate(from_fix=if_else((digits_from %% rep_times) != 0, 10^digits_from, from),
@@ -39,3 +40,23 @@ dat |> find_invalids(rep_times=4) |> print(n=100)
 map_dfr(2:10, \(x) { dat |> find_invalids(x) }) |>
   pull(range) |> unique() |>
   sum()
+
+# alternative: just generate all possible invalids and rule them out
+invalids <- tibble(pattern_reps = 2:10) |>
+  mutate(min_pattern = 1,
+         max_pattern = 10^floor(10/pattern_reps)-1) |>
+  mutate(pattern = map2(min_pattern, max_pattern, seq)) |>
+  unnest(pattern) |>
+  mutate(pattern = map2(pattern, pattern_reps, \(x, y) paste(rep(x, y), collapse=''))) |>
+  mutate(num_pattern = map_dbl(pattern, as.numeric)) |>
+  select(num_pattern) |>
+  unique()
+
+# ok, join our invalids to our ranges and rule them out
+ranges <- read.table("2025/day02/input.txt", sep=",") |>
+  pivot_longer(everything(), names_to="range", values_to="minmax") |>
+  extract(minmax, into=c('from', 'to'), regex="([0-9]+)\\-([0-9]+)", convert=TRUE)
+
+ranges |> cross_join(invalids) |>
+  filter(num_pattern >= from, num_pattern <= to) |>
+  summarise(sum(num_pattern))
